aab8e90de11b1a78939997f77b56bc9da8d05c23
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-07-15 17:53:57 +0800

    feat(footer): add rights section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,34 +1,45 @@
-import styles from './footer.module.css'
+import styles from "./footer.module.css";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <footer>
-      <div className={styles.left}>
-        <h1 className={styles.brandName}>The Market</h1>
-        <p className={styles.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim perferendis quod iure fuga est, corporis unde assumenda aut, numquam, aperiam dolores deleniti dicta sed nulla necessitatibus! Recusandae in nulla sit!</p>
+      <div className={styles.mainContainer}>
+        <div className={styles.left}>
+          <h1 className={styles.brandName}>The Market</h1>
+          <p className={styles.description}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
+            perferendis quod iure fuga est, corporis unde assumenda aut,
+            numquam, aperiam dolores deleniti dicta sed nulla necessitatibus!
+            Recusandae in nulla sit!
+          </p>
+        </div>
+        <div className={styles.right}>
+          <div>
+            <h1 className={styles.heading}>Useful Links</h1>
+            <ul>
+              <li>Home</li>
+              <li>Shop</li>
+              <li>Repository</li>
+              <li>My Github</li>
+            </ul>
+          </div>
+          <div>
+            <h1 className={styles.heading}>Contact</h1>
+            <ul>
+              <li>LinkedIn</li>
+              <li>Email</li>
+              <li>Github</li>
+              <li>Facebook</li>
+            </ul>
+          </div>
+        </div>
       </div>
-      <div className={styles.right}>
-        <div>
-          <h1 className={styles.heading}>Useful Links</h1>
-          <ul>
-            <li>Home</li>
-            <li>Shop</li>
-            <li>Repository</li>
-            <li>My Github</li>
-          </ul>
-        </div>
-        <div>
-          <h1 className={styles.heading}>Contact</h1>
-          <ul>
-            <li>LinkedIn</li>
-            <li>Email</li>
-            <li>Github</li>
-            <li>Facebook</li>
-          </ul>
-        </div>
+      <div className={styles.bottomSection}>
+        <p>@{currentYear} NotRelix. All Rights Reserved.</p>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
