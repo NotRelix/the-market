6f792f8161266922ccdf7be092c43d12712b422f
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-07-17 03:52:42 +0800

    feat(shop): display init products with loading screen

--- src/routes/Root/Root.jsx
+++ src/routes/Root/Root.jsx
@@ -1,14 +1,16 @@
+import { useState } from "react";
 import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
 import styles from "./Root.module.css";
 import { Outlet } from "react-router-dom";
 
 const Root = () => {
+  const [loading, setLoading] = useState(true);
   return (
     <div className={styles.container}>
       <Navbar />
       <main>
-        <Outlet />
+        <Outlet context={{loading, setLoading}} />
       </main>
       <Footer />
     </div>
--- src/routes/ShopPage/ShopPage.jsx
+++ src/routes/ShopPage/ShopPage.jsx
@@ -1,21 +1,35 @@
 import { useEffect, useState } from "react";
 import { fetchProducts } from "../../components/api/api";
+import Loading from "../../components/Loading/Loading";
+import { useOutletContext } from "react-router-dom";
 
 const ShopPage = () => {
+  const { loading, setLoading } = useOutletContext();
   const [products, setProducts] = useState([]);
   useEffect(() => {
     async function loadProducts() {
+      setLoading(true);
       const response = await fetchProducts();
-      setProducts(response);
-      console.log(response);
+      setProducts(response.products);
+      console.log(response.products);
+      setLoading(false);
     }
 
     loadProducts();
-  }, []);
+  }, [setLoading]);
+
+  if (loading) {
+    return(<Loading />)
+  }
+
   return (
-    <div>
-      <h1>Shopping Page</h1>
-    </div>
+    <section>
+      {products.map((product) => (
+        <div>
+          <h1>{product.title}</h1>
+        </div>
+      ))}
+    </section>
   )
 }
 
