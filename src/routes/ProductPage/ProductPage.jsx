0d49f2dea2dce82a5c01aaba03bfe1523e17c7d5
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-07-17 17:41:02 +0800

    feat: add initial layout for ProductPage

--- src/routes/ProductPage/ProductPage.jsx
+++ src/routes/ProductPage/ProductPage.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { useLocation, useOutletContext, useParams } from "react-router-dom"
 import { fetchSingleProduct } from "../../components/api/api";
 import Loading from "../../components/Loading/Loading";
+import styles from './ProductPage.module.css'
+import StarRatings from "react-star-ratings";
 
 const ProductPage = () => {
   const { productId } = useParams();
@@ -26,11 +28,30 @@ const ProductPage = () => {
   if (loading) return <Loading />
 
   return (
-    <section>
-      <h1>{product.title}</h1>
-      <h1>{product.title}</h1>
-      <h1>{product.title}</h1>
-      <h1>{product.title}</h1>
+    <section className={styles.container}>
+      <div className={styles.cardContainer}>
+        <div className={styles.imageContainer}>
+          <img className={styles.image} src={product.image} alt={product.title} />
+        </div>
+        <div className={styles.textArea}>
+          <h1>{product.title}</h1>
+          <div className={styles.rating}>
+            <StarRatings
+              className={styles.star}
+              rating={product.rating.rate}
+              starRatedColor={'rgb(255, 196, 0)'}
+              starDimension={'20px'}
+              starSpacing='0px'
+            />
+            <span>{product.rating.rate}</span>
+            <span>|</span>
+            <span>Ratings: {product.rating.count}</span>
+          </div>
+          <span>Category: {product.category}</span>
+          <span>{product.price}</span>
+          <span>{product.description}</span>
+        </div>
+      </div>
     </section>
   )
 }
