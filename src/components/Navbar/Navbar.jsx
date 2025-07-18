a48484ce6fd1bb599631cf47f240972eb743fdc2
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-07-18 13:12:48 +0800

    feat: add initial layout for shopping cart slider

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import styles from "./Navbar.module.css";
-import { Menu, ShoppingCart } from "lucide-react";
-import PropTypes from "prop-types";
+import { Menu, ShoppingCart, X } from "lucide-react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
@@ -17,8 +16,13 @@ const navLinks = [
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [cartOpen, setCartOpen] = useState(false);
   function toggleMenu() {
     setMenuOpen(!menuOpen);
+  }
+
+  function toggleCart() {
+    setCartOpen(!cartOpen);
   }
   return (
     <>
@@ -34,15 +38,22 @@ const Navbar = () => {
               {navLink.name}
             </Link>
           ))}
-          <ShoppingCart className={styles.shoppingCart} />
+          <ShoppingCart onClick={toggleCart} className={styles.shoppingCart} />
         </div>
       </nav>
       <Menu onClick={toggleMenu} className={styles.menu} />
       <div className={`${styles.menuSlider} ${(menuOpen) ? "" : styles.hidden}`}>
+        <hr />
         <Link to={'/'} className={styles.menuLink} onClick={toggleMenu} >Home</Link>
         <Link to={'shop'} className={styles.menuLink} onClick={toggleMenu}>Shop</Link>
       </div>
       <div onClick={() => setMenuOpen(false)} className={`${styles.backdrop} ${(menuOpen) ? "" : styles.backdropHidden}`}></div>
+      <div className={`${styles.cartSlider} ${(cartOpen) ? "" : styles.hidden}`}>
+        <X className={styles.closeCart} onClick={() => setCartOpen(false)} />
+        <hr />
+        {/* Cart Items */}
+      </div>
+      <div onClick={() => setCartOpen(false)} className={`${styles.cartBackdrop} ${(cartOpen) ? "" : styles.backdropHidden}`}></div>
     </>
   );
 };
